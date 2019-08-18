d1577f25d81c3f716e5e8546cc0a9e6093c01a07
Nick Zhao <Nick.Zhao@GMR.local>
2019-08-18 16:31:35 -0700

    Need to finish undirected part of find

---
+++ Union_Find/Union_Find/Graph.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Union_Find
+{
+    class Graph<T> where T : IComparable
+    {
+        public List<Vertex<T>> Vertices = new List<Vertex<T>>();
+        public int EdgeCount { get; private set; }
+
+        public Graph()
+        {
+        }
+
+        public void AddVertex(Vertex<T> vert)
+        {
+            Vertices.Add(vert);
+        }
+
+        public bool RemoveVertex(Vertex<T> vertex)
+        {
+            foreach(var vert in Vertices)
+            {
+                vert.Edges.Remove(vertex);
+            }
+            return Vertices.Remove(vertex);
+        }
+
+        public void AddEdge(T a, T b, double weight)
+        {
+            var x = GetVertex(a);
+            var y = GetVertex(b);
+
+            if(x == null || y == null)
+            {
+                throw new Exception("Vertices Not Found");
+            }
+
+            AddEdge(x, y, weight);
+        }
+
+        public Vertex<T> GetVertex(T value)
+        {
+            foreach(var vert in Vertices)
+            {
+                if(vert.Value.Equals(value))
+                {
+                    return vert;
+                }
+            }
+            return null;
+        }
+    }
+
+
+
+}
---
+++ Union_Find/Union_Find/Vertex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Union_Find
+{
+    class Vertex<T> where T : IComparable
+    {
+        public T Value;
+        public bool IsVisited;
+        public double TotalDistance = double.NegativeInfinity;
+        public Vertex<T> Founder = null;
+
+        public Dictionary<Vertex<T>, double> Edges = new Dictionary<Vertex<T>, double>();
+
+        public Vertex(T value)
+        {
+            this.Value = value;
+            Edges = new Dictionary<Vertex<T>, double>();
+        }
+
+        public int CompareTo(Vertex<T> other)
+        {
+            return TotalDistance.CompareTo(other.TotalDistance);
+        }
+        
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
